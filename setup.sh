2f08f5073d0be537b8f5df71ebfc65692b9f6b2c
Knowledge-Wisdom-Understanding <36655901+Knowledge-Wisdom-Understanding@users.noreply.github.com>
2019-06-14 06:07:47 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+# Check if user is root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+echo '[+] Running: apt-get update -y'
+apt-get update -y
+
+echo '[+] Downloading Dependencies'
+apt-get install steghide -y
+pip3 install stegcracker
+
+echo '[+] Congratulations, All tools installed successfully!'
